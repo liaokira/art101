c5b3a973e5e689bd54414fdbc56200f815870a5e
littlemothh <sydneyy.2004@gmail.com>
2024-04-25 13:03:07 -0700

    lab5

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,21 +1,20 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Super Turbo Turkey Puncher 3
+// An implementation of the game
+// syd young <symyoung@ucsc.edu>
+// April 2024
 
-// Constants
+//setup
+  // Start with no money and the turkey healthy.
+//Keep Going Until Something Happens: 
+  //Keep track of how much money you have and how much health the turkey has.
+  //If you have enough money or the turkey's health runs out, stop.
+//During Each Round: 
+  //Check how much money you have and how healthy the turkey is.
+  //If you want to punch the turkey, go ahead.
+  //If you punch, the turkey loses some health, and you earn some money.
+// End of the Game
+  //If you've earned enough money, hooray! You win.
+  //If the turkey's health drops to zero, you've won as well.
+//The End 
 
-// Functions
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
-
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,21 +1,20 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - intro to javascript
+// Author: syd young + noah dane
+// Date:25 april
 
-// Constants
+//Define variables
+  make = "Honda";
+  model = "Element";
+  year = 2008;
+  color = "light blue"
 
-// Functions
+//Calculate
+  age = 2024 - year;
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
-  // some code here
-  // return results;
-}
+//Output
+  document.writeln("Make: " + make  + "<br>");
+  document.writeln("Model: " + model  + "<br>");
+  document.writeln("Color: " + color  + "<br>");
+  document.writeln("Year: " + year  + "<br>");
+  document.writeln("Age: " + age  + "years<br>");
 
-function main() {
-  console.log("Main function started.");
-  // the code that makes everything happen
-}
-
-// let's get this party started
-main();
