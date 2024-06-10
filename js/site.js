72d722c41602fbb2b2eb8dc6457807ea0f1ebfae
littlemothh <sydneyy.2004@gmail.com>
2024-06-10 01:15:57 -0700

    final submission

--- js/site.js
+++
@@ -1,7 +0,0 @@
-var splashScreen = document.querySelector('.splash');
-splashScreen.addEventListener('click',()=>{
-  splashScreen.style.opacity = 0;
-  setTimeout(()=>{
-    splashScreen.classList.add('hidden')
-  },610)
-})
